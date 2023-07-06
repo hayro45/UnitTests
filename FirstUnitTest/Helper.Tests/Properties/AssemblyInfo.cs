6e366347ce02f927b9e0ea0dfd81e8d21d21eb8e
hayro45 <hayroxgitar47@gmail.com>
2023-07-06 06:08:40 +0300

    first unit test

---
+++ FirstUnitTest/Helper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helper.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helper.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ecb9df51-c40b-4f45-84ac-fec3d35fd555")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FirstUnitTest/Helper.Tests/StringHelperTests.cs
@@ -0,0 +1,44 @@
+﻿using Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper.Tests
+{
+    [TestClass]
+    public class StringHelperTests
+    {
+        [TestMethod]
+        public void girilen_ifadenin_icindeki_fazla_bosluklar_silinecek()
+        {
+            //Arrange
+            string ifade = " Hayrettin   Dal";
+            string beklenen = "Hayrettin Dal";
+
+
+            //Act
+            var gerceklesen = StringHelper.BosluklariSil(ifade);
+
+            //Assert
+            Assert.AreEqual(beklenen, gerceklesen);
+        }
+        [TestMethod]
+        public void girilen_ifadenin_basindaki_ve_sonundaki_bosluklar_silinecek()
+        {
+            //Arrange
+            string ifade = "      Hayrettin     Dal     ";
+            string beklenen = "Hayrettin Dal";
+
+
+            //Act
+            var gerceklesen = StringHelper.BosluklariSil(ifade);
+
+            //Assert
+            Assert.AreEqual(beklenen, gerceklesen);
+        }
+    }
+}
